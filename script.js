866516e5a76fbcec96d81949cbe2942e4e669efd
ajjawi_amer@hotmail.com <ajjawi_amer@hotmail.com>
2025-06-24 12:44:19 -0400

    fixed up javascript code for the grid

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 //input variable
 const userInput = document.querySelector("#userInput");
 
-
 //grid div variable
 const grid = document.querySelector(".grid");
 
@@ -14,28 +13,40 @@ let convertToInt;
 //number variable
 let number = 0;
 
-
 //function get userValue
 function getUserValue() {
   button.addEventListener("click", function(e) {
+
     e.preventDefault();
-
-
     convertToInt = parseInt(userInput.value);
-    number = convertToInt;
-    
-    if (isNaN(number)) {
-      number = 16;
-    } 
-    
-    return number;
-    
-  })
+    number = isNaN(convertToInt) ? 16 : convertToInt;
+     
+    clearGrid();
+    makeGrid(number);
+    // return number;
+    console.log(number);
+  });
+ 
 }
 getUserValue();
-
-
 //function to make grid
-function makeGrid(){
-  
+function makeGrid(number) {
+  for(let i = 0; i < number; i++) {
+    let rows = document.createElement("tr");
+     rows.classList.add("rows");
+    for (j = 0; j < number; j++) {
+      let cols = document.createElement("td");
+      cols.classList.add("cols");
+      rows.appendChild(cols);
+    }
+   
+    grid.appendChild(rows);
+  }
+
 }
+
+//clear grid
+function clearGrid() {
+  grid.textContent = "";
+}
+clearGrid();
